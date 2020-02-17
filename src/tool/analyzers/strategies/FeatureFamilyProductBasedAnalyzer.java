331bc43941742e7f2e92c8c93e8c8c6c59132295
brenoafb <breno.afb@gmail.com>
2020-02-17 11:49:14 -0300

    Add some experimental code.

--- src/tool/analyzers/strategies/FeatureFamilyProductBasedAnalyzer.java
+++ src/tool/analyzers/strategies/FeatureFamilyProductBasedAnalyzer.java
@@ -23,7 +23,11 @@ import tool.stats.IFormulaCollector;
 import tool.stats.ITimeCollector;
 import expressionsolver.Expression;
 import expressionsolver.ExpressionSolver;
+import org.nfunk.jep.JEP;
+import org.nfunk.jep.Node;
 
+import java.util.HashMap;
+import java.util.Map;
 /**
  * Orchestrator of feature-family-product-based analyses.
  */
@@ -46,11 +50,63 @@ public class FeatureFamilyProductBasedAnalyzer {
     public IReliabilityAnalysisResults evaluateReliability(RDGNode node, ConcurrencyStrategy concurrencyStrategy) throws CyclicRdgException {
     	List<RDGNode> dependencies = node.getDependenciesTransitiveClosure();
     	
-    	List<Component<String>> expressions = firstPhase.getReliabilityExpressions(dependencies, concurrencyStrategy);
+    	List<Component<String>> components = firstPhase.getReliabilityExpressions(dependencies, concurrencyStrategy);
     	
+    	int n = components.size();
+    	List<String> variables = components.stream().map(e -> e.getId()).collect(Collectors.toList());
+    	// List<String> variables = components.stream().map(e -> e.getId()).collect(Collectors.toList());
+    	List<String> expressions = components.stream().map(e -> e.getAsset()).collect(Collectors.toList());
+    	List<String> pcs = components.stream().map(e -> e.getPresenceCondition()).collect(Collectors.toList());
+    	Map<String,String> var2exp = new HashMap<String,String>();
+    	Map<String, String> var2pc = new HashMap<String, String>();
+    	Map<String,Double> var2val = new HashMap<String, Double>();
+    	Map<String,String> var2ite = new HashMap<String,String>();
+
+    	for (int i = 0; i < n; i++) {
+    		var2exp.put(variables.get(i), expressions.get(i));
+    		var2pc.put(variables.get(i), pcs.get(i));
+    	}
+
+    	JEP parser = new JEP();
+    	JEP parserStr = new JEP();
+
+    	for (String variable : variables) {
+    		System.out.println("Adding variable " + variable + " as " + var2exp.get(variable));
+    		parser.parseExpression(var2exp.get(variable));
+    		Double value = parser.getValue();
+    		var2val.put(variable, value);
+    		parser.addVariableAsObject(variable, value);
+    		
+    	}
+        
+    	/* get the ite expressions */
+    	for (int i = 0; i < n; i++) {
+    		String var = variables.get(i);
+    		String exp = var2exp.get(var);
+    		
+    		for (int j = 0; j < i; j++) {
+    			String varPrev = variables.get(j);
+    			String newExp = exp.replace(varPrev, "(" + var2ite.get(varPrev) + ")");
+    			exp = newExp;
+    		}
+    		
+    		String ite = getITEExpression(var, exp);
+    		var2ite.put(var, ite);
+    	}
+    	System.out.println("parsing expression " + var2exp.get("BSN"));
+    	parser.parseExpression("BSN");
+    	Node p = parser.getTopNode();
+    	System.out.println(parser.getValueAsObject());
+    	System.out.println(parser.getErrorInfo());
+
+    	System.out.println(var2ite.get("BSN"));
+
     	/* TODO: Variability encoding */
-    	
     	/* TODO: Product-based step */
         return null;
+    }
+    
+    private String getITEExpression(String variable, String expression) {
+    	return variable + "*(" + expression + ") + (1-" + variable + ")";
     }
 }
--- src/ui/Options.java
+++ src/ui/Options.java
@@ -91,7 +91,7 @@ class Options {
                                  + "FEATURE_FAMILY_PRODUCT (feature-family-product-based).")
                 .withRequiredArg()
                 .ofType(AnalysisStrategy.class)
-                .defaultsTo(AnalysisStrategy.FAMILY_PRODUCT)
+                .defaultsTo(AnalysisStrategy.FEATURE_FAMILY_PRODUCT)
                 .describedAs("FEATURE_FAMILY | FEATURE_PRODUCT | FAMILY | FAMILY_PRODUCT | PRODUCT | FEATURE_FAMILY_PRODUCT");
 
         OptionSpec<Void> helpOption = optionParser
