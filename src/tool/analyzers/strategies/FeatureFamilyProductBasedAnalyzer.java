807c6ec392b7ab3265b407404e28db2666fc83c4
brenoafb <breno.afb@gmail.com>
2020-02-22 03:51:22 -0300

    Add java code implementing analysis steps.

--- src/tool/analyzers/strategies/FeatureFamilyProductBasedAnalyzer.java
+++ src/tool/analyzers/strategies/FeatureFamilyProductBasedAnalyzer.java
@@ -65,96 +65,80 @@ public class FeatureFamilyProductBasedAnalyzer {
     	
     	/* preparing variables */
     	int n = components.size();
-    	List<String> variables = components.stream().map(e -> e.getId()).collect(Collectors.toList());
+    	List<String> variables = dependencies.stream().map(e -> e.getId()).collect(Collectors.toList());
     	List<String> expressions = components.stream().map(e -> e.getAsset()).collect(Collectors.toList());
     	List<String> pcs = components.stream().map(e -> e.getPresenceCondition()).collect(Collectors.toList());
     	Map<String,String> var2exp = new HashMap<String,String>();
     	Map<String, String> var2pc = new HashMap<String, String>();
-
-  		PrintWriter writer;
-		try {
-			writer = new PrintWriter("/home/breno/tmp/formulas.txt", "UTF-8");
-			for (int i = 0; i < n; i++) {
-				String var = variables.get(i);
-				String exp = expressions.get(i);
-				String pc = pcs.get(i);
-    		
-				var2exp.put(var, exp);
-				var2pc.put(var, pc);
-    		
-				writer.println(var + "\n" + exp);
-			}
-			writer.close();
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (UnsupportedEncodingException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-        
+    	Map<String, String> var2ite = new HashMap<String, String>();
+    	
     	/* expression variability encoding */
+		for (int i = 0; i < n; i++) {
+			String var = variables.get(i);
+			String exp = expressions.get(i);
+			String pc = pcs.get(i);
+			
+			exp = preprocessExpression(exp);
+    		
+			var2exp.put(var, exp);
+			var2pc.put(var, pc);
+			
+			for (int j = 0; j < i; j++) {
+				String varSubs = variables.get(j);
+				exp = exp.replaceAll(varSubs + " ", var2ite.get(varSubs));
+			}
+			
+			var2ite.put(var, getITE(var, exp));
+		}
 
+		String rootVar = variables.get(n-1);
+		String iteExpression = var2ite.get(rootVar);
+		
     	/* product iteration step */
-    	int i = 0;
     	Map<Collection<String>, Double> results = new HashMap<Collection<String>, Double>();
     	List<Collection<String>> configList = configurations.collect(Collectors.toList());
-    	List<String> paths = new LinkedList<String>();
     	for (Collection<String> config : configList) {
-    		Map <String, Integer> var2value = new HashMap<String, Integer>();
+    		Map <String, Double> var2value = new HashMap<String, Double>();
     		for (String var : config) {
-    			var2value.put(var, 1);
+    			var2value.put(var, 1.0);
     		}
     		for (String var : variables) {
     			String pc = var2pc.get(var);
     			if (pc == "true") {
-    				var2value.put(var, 1);
+    				var2value.put(var, 1.0);
     			} else if (var2value.containsKey(pc)) {
     				var2value.put(var, var2value.get(pc));
     			} else {
-    				var2value.put(var, 0);
+    				var2value.put(var, 0.0);
     			}
     		}
 
-    		PrintWriter valueWriter;
-			try {
-				String path = "/home/breno/tmp/values-" + i + ".txt";
-				valueWriter = new PrintWriter(path, "UTF-8");
-				variables.forEach(v -> valueWriter.println(v + "\n" + var2value.get(v)));
-				valueWriter.close();
-				paths.add(path);
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			} catch (UnsupportedEncodingException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-    		i++;
+    		JEP parser = new JEP();
+    		variables.forEach(v -> parser.addVariable(v, var2value.get(v)));
+    		parser.parseExpression(iteExpression);
+    		Double reliability = parser.getValue();
+    		if (parser.hasError()) {
+    			System.out.println("Parser error: " + parser.getErrorInfo());
+    		}
+    		results.put(config, reliability);
     	}
     	
     	// call program
-    	String command = "/home/breno/Documents/formula-parser/TestLang /home/breno/tmp/formulas.txt";
-    	for (String path : paths) {
-    		command += " " + path;
-    	}
 
-    	command += " /home/breno/tmp/output.txt";
-    	
-    	Runtime rt = Runtime.getRuntime();
-    	try {
-			Process program = rt.exec(command);
-			int exitCode = program.waitFor();
-			System.out.println("Exit code: " + exitCode);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-    	// TODO read results
         return new MapBasedReliabilityResults(results);
+    }
+    
+    private String preprocessExpression(String exp) {
+    	exp = exp.replaceAll("\\(", " ( ");
+    	exp = exp.replaceAll("\\)", " ) ");
+    	exp = exp.replaceAll("\\+", " + ");
+    	exp = exp.replaceAll("\\-", " - ");
+    	exp = exp.replaceAll("\\*", " * ");
+    	exp = exp.replaceAll("\\/", " / ");
+    	return exp;
+    }
+
+    private String getITE(String var, String exp) {
+    	return " ( " + var + " * ( " + exp + " ) + ( 1 - " + var + " ) ) ";
     }
 }
