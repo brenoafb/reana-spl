b0a219b735a5da4066a6a9a68d213fbfe74ecfcb
brenoafb <breno.afb@gmail.com>
2020-05-15 14:18:53 -0300

    Add function to get libc path in a portable manner.

--- src/jadd/CUtils.java
+++ src/jadd/CUtils.java
@@ -1,6 +1,8 @@
 package jadd;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,35 +11,54 @@ import org.bridj.NativeLibrary;
 import org.bridj.Pointer;
 
 class CUtils {
-    private static final Logger LOGGER = Logger.getLogger(CUtils.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(CUtils.class.getName());
 
-    private static NativeLibrary libc;
-    private static DynamicFunction<Pointer<?>> fopenHandle;
-    private static DynamicFunction<?> fcloseHandle;
+	private static NativeLibrary libc;
+	private static DynamicFunction<Pointer<?>> fopenHandle;
+	private static DynamicFunction<?> fcloseHandle;
 
-    public static final String ACCESS_WRITE = "w";
+	public static final String ACCESS_WRITE = "w";
 
-    static {
-        try {
-            // TODO Make libc's path portable!
-            libc = NativeLibrary.load("/lib/x86_64-linux-gnu/libc.so.6");
+	static {
+		try {
+			String path;
+			try {
+				path = getLibcPath();
+			} catch (InterruptedException | IOException e) {
+				path = "/lib/x86_64-linux-gnu/libc.so.6";
+			}
 
-            Pointer<?> fopenAddress = libc.getSymbolPointer("fopen");
-            fopenHandle = fopenAddress.asDynamicFunction(null, Pointer.class, Pointer.class, Pointer.class);
+			System.out.println("libc path: " + path);
+			libc = NativeLibrary.load(path);
 
-            Pointer<?> fcloseAddress = libc.getSymbolPointer("fclose");
-            fcloseHandle = fcloseAddress.asDynamicFunction(null, int.class, Pointer.class);
-        } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, e.toString(), e);
-        }
-    }
+			Pointer<?> fopenAddress = libc.getSymbolPointer("fopen");
+			fopenHandle = fopenAddress.asDynamicFunction(null, Pointer.class, Pointer.class, Pointer.class);
 
-    public static Pointer<?> fopen(String fileName, String access) {
-        return fopenHandle.apply(Pointer.pointerToCString(fileName),
-                                 Pointer.pointerToCString(access));
-    }
+			Pointer<?> fcloseAddress = libc.getSymbolPointer("fclose");
+			fcloseHandle = fcloseAddress.asDynamicFunction(null, int.class, Pointer.class);
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, e.toString(), e);
+		}
+	}
 
-    public static int fclose(Pointer<?> openFile) {
-        return (int) fcloseHandle.apply(openFile);
-    }
+	public static Pointer<?> fopen(String fileName, String access) {
+		return fopenHandle.apply(Pointer.pointerToCString(fileName),
+				Pointer.pointerToCString(access));
+	}
+
+	public static int fclose(Pointer<?> openFile) {
+		return (int) fcloseHandle.apply(openFile);
+	}
+
+	private static String getLibcPath() throws IOException, InterruptedException {
+		Runtime rt = Runtime.getRuntime();
+		Process pr = rt.exec("gcc --print-file-name=libc.so.6");
+
+		BufferedReader stdInput = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+		String path = stdInput.readLine();
+		int exitCode = pr.waitFor();
+
+		System.out.println("Exit code: " + exitCode);
+		return path;
+	}
 }
